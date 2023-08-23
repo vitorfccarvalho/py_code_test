0634ca2e062cbb4719e393451e809034f221d3a2
vitorfccarvalho <vitorfassanaro@gmail.com>
2023-08-22 21:04:59 -0300

    First Commit with functional code and all TCs working

---
+++ calculator.py
@@ -0,0 +1,16 @@
+class Calculator():
+
+    def addition(self, a, b):
+        return a + b
+
+    def addition3(self, a, b, c):
+        return a + b + c
+    
+    def subtraction(self, a, b):
+        return a - b
+    
+    def multiplication(self, a, b):
+        return a * b
+    
+    def division(self, a, b):
+        return a / b
